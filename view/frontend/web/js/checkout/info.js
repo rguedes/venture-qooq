950de268bc8946d6ebefe756b05bec06a058a821
RGuedes <geral@rolandoguedes.com>
2016-12-23 12:09:06 +0000

    checkout

---
+++ view/frontend/web/js/checkout/info.js
@@ -0,0 +1,56 @@
+define(
+    [
+        'ko',
+        'uiComponent',
+        'underscore',
+        'Magento_Checkout/js/model/step-navigator'
+    ],
+    function (
+        ko,
+        Component,
+        _,
+        stepNavigator
+    ) {
+        'use strict';
+        /**
+         *
+         * mystep - is the name of the component's .html template,
+         * my_module - is the name of the your module directory.
+         *
+         */
+        return Component.extend({
+            defaults: {
+                template: 'Venture_Qooq/checkout/resume/info'
+            },
+
+            //add here your logic to display step,
+            isVisible: ko.observable(true),
+
+            /**
+             *
+             * @returns {*}
+             */
+            initialize: function () {
+                this._super();
+
+                return this;
+            },
+
+            /**
+             * The navigate() method is responsible for navigation between checkout step
+             * during checkout. You can add custom logic, for example some conditions
+             * for switching to your custom step
+             */
+            navigate: function () {
+
+            },
+
+            /**
+             * @returns void
+             */
+            navigateToNextStep: function () {
+                stepNavigator.next();
+            }
+        });
+    }
+);
